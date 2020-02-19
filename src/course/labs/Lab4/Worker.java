043649d3e765a46cc98e92cadf9fecaf35dbb9c6
TeryVeneno <40150870+TeryVeneno@users.noreply.github.com>
2020-02-19 08:55:24 -0600

    Worker Class Update

--- src/course/labs/Lab4/Worker.java
+++ src/course/labs/Lab4/Worker.java
@@ -1,4 +1,45 @@
 package course.labs.Lab4;
 
 public class Worker {
+
+    private TimeCard time_card;
+    private Equipment equipment;
+    private String type;
+    private String name;
+    private String address;
+
+
+    public Worker (float salary, String type, String name, String address) {
+        // create time_card, salary
+        time_card = new TimeCard();
+        this.type = type;
+        this.name = name;
+        this.address = address;
+    }
+
+    // To be finished
+    public float get_salary (int shifts) {
+        // time_card method returns salary based on number of shifts
+        return 0;
+    }
+
+    public void set_equipment (Equipment e) {
+        equipment = e;
+    }
+
+    public String get_name () {
+        return name;
+    }
+
+    public String get_type () {
+        return type;
+    }
+
+    public String get_address () {
+        return address;
+    }
+
+    public Equipment get_equipment () {
+        return equipment;
+    }
 }
