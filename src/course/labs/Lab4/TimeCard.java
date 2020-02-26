b863df22462f118a708a28fb4d8d356d5f8f5102
RCHS <RCHS@10.48.11.99>
2020-02-26 09:37:46 -0600

    TimeCard Class Update

--- src/course/labs/Lab4/TimeCard.java
+++ src/course/labs/Lab4/TimeCard.java
@@ -1,4 +1,39 @@
 package course.labs.Lab4;
 
 public class TimeCard {
+	
+
+	  private boolean isworking;
+	   private ArrayList<Shift> shifts = new ArrayList<>();
+	   
+	   private float salary;
+	   
+	   public TimeCard(float salary)
+	   	   {
+		this.salary=salary;
+	   	   }
+	   
+	   public float getSalary() 
+	   {
+		   return shifts.size()*salary;
+	   }
+	   
+	   public boolean isworking()
+	   {
+		   return isworking;
+	   }
+	   
+	   public void setWorking(boolean a)
+	   {
+		   isworking=a;
+	   }
+	   
+	   public void addShift(String projectname, int time)
+	   {
+		   shifts.add(new Shift(projectname,time))
+	   }
+	  
+	  
+	  
+   }
 }
