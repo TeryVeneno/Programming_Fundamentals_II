6d9f12393c63d1309996922b9d6bbb7728e18245
TeryVeneno <40150870+TeryVeneno@users.noreply.github.com>
2020-02-26 09:10:37 -0600

    Construction Side Class Update

--- src/course/labs/Lab4/ConstructionTeam.java
+++ src/course/labs/Lab4/ConstructionTeam.java
@@ -1,4 +1,9 @@
 package course.labs.Lab4;
 
+import java.util.ArrayList;
+
 public class ConstructionTeam {
+    Project project;
+    ArrayList<Worker> workers = new ArrayList<>();
+    Foreman foreman;
 }
