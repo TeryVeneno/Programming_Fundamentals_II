a1581c4e328b7d2401874c21fe5068828ee9948d
TeryVeneno <40150870+TeryVeneno@users.noreply.github.com>
2020-02-26 09:10:17 -0600

    TimeCard Class Update

---
+++ src/course/labs/Lab4/Shift.java
@@ -0,0 +1,11 @@
+package course.labs.Lab4;
+
+public class Shift {
+    public final String project_name;
+    public final int shift;
+
+    public Shift (String pn, int s) {
+        project_name = pn;
+        shift = s;
+    }
+}
