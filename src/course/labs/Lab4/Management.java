28b96c1a90a030ca0185e189780e151dfca5141e
ajelet12 <61241631+ajelet12@users.noreply.github.com>
2020-02-26 18:18:31 -0600

    Management Class Update

--- src/course/labs/Lab4/Management.java
+++ src/course/labs/Lab4/Management.java
@@ -1,4 +1,24 @@
 package course.labs.Lab4;
-
-public class Management {
+//class made by Ajelet && Mateilda 
+public class Management 
+{
+	private ArrayList<Equipment> equipments = new ArrayList<>();
+	
+	public Equipment request_equipment(String type)
+	{
+		for (int i = 0; i < equipments.size(); i++) {
+			if (equipments.get(i).get_type()==type) 
+			{
+				Equipment temp = equipments.get(i);
+				equipments.remove(i);
+				return temp;
+			}
+		}
+		return null;
+	}
+	
+	public void return_equipment(Equipment e) {
+		equipments.add(e);
+	}
+	
 }
